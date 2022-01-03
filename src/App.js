dae07e1d67f80053f8790793a6b5fd804229eb7e
Durgesh-Selkari-1 <durgesh.selkari@iviewlabs.net>
2022-01-03 18:32:17 +0530

    Contact-form-final

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ContactForm from './Components/ContactForm';
-import ContactForm2 from './Components/ContactForm2';
-import ContactForm3 from './Components/ContactForm3';
-
-import ContactForm4 from './Components/ContactForm4';
-
 function App() {
   return (
    <>
- <ContactForm2/>
+ <ContactForm/>
    </>
   );
 }
